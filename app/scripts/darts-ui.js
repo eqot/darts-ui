a1d9e21bb67b3411e989d935f132a22809386d34
Ikuo Terado <eqobar@gmail.com>
2014-02-10 00:49:57 +0900

    Refactor code with CSS

--- app/scripts/darts-ui.js
+++ app/scripts/darts-ui.js
@@ -10,38 +10,31 @@ var DartsUi = function (element) {
   this.centerY = 301;
   this.radius  = 300;
 
-  this.colorHigh0 = '#f00';
-  this.colorHigh1 = '#00f';
-  this.colorLow0  = '#000';
-  this.colorLow1  = '#fff';
-  this.borderColor = '#888';
-
   this.points = [
     20, 1, 18, 4, 13, 6, 10, 15, 2, 17, 3, 19, 7, 16, 8, 11, 14, 9, 12, 5
   ];
 };
 
 DartsUi.prototype.draw = function() {
-  var border = this.s.circle(this.centerX, this.centerY, this.radius);
-  border.attr({
-    fill: '#000',
-    stroke: this.borderColor,
-    strokeWidth: 1
+  var base = this.s.circle(this.centerX, this.centerY, this.radius);
+  base.attr({
+    class: 'base'
   });
 
-  var doubleRings  = this.drawRings(this.radius * 0.75, this.radius * 0.05, this.colorHigh0, this.colorHigh1);
-  var singleRingsO = this.drawRings(this.radius * 0.60, this.radius * 0.25, this.colorLow0,  this.colorLow1);
-  var tripleRings  = this.drawRings(this.radius * 0.45, this.radius * 0.05, this.colorHigh0, this.colorHigh1);
-  var singleRingsI = this.drawRings(this.radius * 0.25, this.radius * 0.35, this.colorLow0,  this.colorLow1);
+  var doubleRings  = this.drawRings('double high-ring',    this.radius * 0.75, this.radius * 0.05);
+  var singleRingsO = this.drawRings('single single-outer', this.radius * 0.60, this.radius * 0.25);
+  var tripleRings  = this.drawRings('triple high-ring',    this.radius * 0.45, this.radius * 0.05);
+  var singleRingsI = this.drawRings('single single-inner', this.radius * 0.25, this.radius * 0.35);
 
-  var OuterBull    = this.drawBull(this.radius * 0.1,  this.colorHigh0);
-  var BullsEye     = this.drawBull(this.radius * 0.05, this.colorLow0);
+  var OuterBull    = this.drawBull('bull bull-outer', this.radius * 0.1);
+  var BullsEye     = this.drawBull('bull bull-inner', this.radius * 0.05);
 
-  var points = this.drawPoints(this.radius * 0.9, this.radius * 0.1, '#fff');
+  var points = this.drawPoints('point', this.radius * 0.9, this.radius * 0.1, '#fff');
 };
 
-DartsUi.prototype.drawRings = function(radius, strokeWidth, color0, color1) {
-  var rings = [];
+DartsUi.prototype.drawRings = function(className, radius, strokeWidth) {
+  var rings = this.s.g().attr({class: className});
+
   for (var i = 0; i < 20; i++) {
     var angle0 = (i * 18 - 9) * Math.PI / 180;
     var angle1 = (i * 18 + 9) * Math.PI / 180;
@@ -49,45 +42,44 @@ DartsUi.prototype.drawRings = function(radius, strokeWidth, color0, color1) {
     var y0 = this.centerY - radius * Math.cos(angle0);
     var x1 = this.centerX + radius * Math.sin(angle1);
     var y1 = this.centerY - radius * Math.cos(angle1);
-    var ring = this.s.path("M" + x0 + " " + y0 + " A" + radius + " " + radius + " 0 0 1 " + x1 + " " + y1);
+    var ring = this.s.path('M' + x0 + ' ' + y0 + ' A' + radius + ' ' + radius + ' 0 0 1 ' + x1 + ' ' + y1);
     ring.attr({
-      stroke: ((i & 1) === 0) ? color0 : color1,
+      class: 'ring',
       strokeWidth: strokeWidth
     });
 
-    rings.push(ring);
+    rings.append(ring);
   }
 
   return rings;
 };
 
-DartsUi.prototype.drawBull = function(radius, color) {
+DartsUi.prototype.drawBull = function(className, radius) {
   var bull = this.s.circle(this.centerX, this.centerY, radius);
   bull.attr({
-    fill: color,
-    stroke: this.borderColor,
-    strokeWidth: 1
+    class: className
   });
 
   return bull;
 };
 
-DartsUi.prototype.drawPoints = function(radius, fontSize, color) {
-  var points = [];
+DartsUi.prototype.drawPoints = function(className, radius) {
+  var points = this.s.g().attr({class: 'points'});
+
   for (var i = 0; i < 20; i++) {
     var angle = (i * 18) * Math.PI / 180;
     var x = this.centerX + radius * Math.sin(angle);
     var y = this.centerY - radius * Math.cos(angle);
     var point = this.s.text(x, y, this.points[i]);
     point.attr({
-      fill: color,
-      dy: fontSize / 2,
-      'font-family': 'Impact',
-      'font-size': fontSize + 'px',
-      'text-anchor': 'middle'
+      class: className
+    });
+    var height = point.getBBox().height;
+    point.attr({
+      dy: height / 2.8
     });
 
-    points.push(point);
+    points.append(point);
   }
 
   return points;

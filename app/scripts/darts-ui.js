8133ece35acd4a9557bf1f3f2d904972762106de
Ikuo Terado <eqobar@gmail.com>
2014-02-09 12:19:33 +0900

    Draw points

--- app/scripts/darts-ui.js
+++ app/scripts/darts-ui.js
@@ -15,6 +15,10 @@ var DartsUi = function (element) {
   this.colorLow0  = '#000';
   this.colorLow1  = '#fff';
   this.borderColor = '#888';
+
+  this.points = [
+    20, 1, 18, 4, 13, 6, 10, 15, 2, 17, 3, 19, 7, 16, 8, 11, 14, 9, 12, 5
+  ];
 };
 
 DartsUi.prototype.draw = function() {
@@ -31,6 +35,8 @@ DartsUi.prototype.draw = function() {
   var singleRingsI = this.drawRing(this.radius * 0.25, this.radius * 0.35, this.colorLow0,  this.colorLow1);
   var bullO = this.drawBull(this.radius * 0.1,  this.colorHigh0);
   var bullI = this.drawBull(this.radius * 0.05, this.colorLow0);
+
+  var points = this.drawPoints(this.radius * 0.9, this.radius * 0.1, '#fff');
 };
 
 DartsUi.prototype.drawRing = function(radius, strokeWidth, color0, color1) {
@@ -63,4 +69,24 @@ DartsUi.prototype.drawBull = function(radius, color) {
   });
 
   return bull;
+};
+
+DartsUi.prototype.drawPoints = function(radius, fontSize, color) {
+  var points = [];
+  for (var i = 0; i < 20; i++) {
+    var angle = (i * 18) * Math.PI / 180;
+    var x = this.centerX + radius * Math.sin(angle);
+    var y = this.centerY - radius * Math.cos(angle);
+    var point = this.s.text(x, y, this.points[i]);
+    point.attr({
+      fill: color,
+      dy: fontSize / 2,
+      'font-size': fontSize + 'px',
+      'text-anchor': 'middle'
+    });
+
+    points.push(point);
+  }
+
+  return points;
 };

eebab40adc080dc94ec078e0a19c729cdd3c77db
Ikuo Terado <eqobar@gmail.com>
2014-02-12 22:44:37 +0900

    Refactor CSS

--- app/scripts/darts-ui.js
+++ app/scripts/darts-ui.js
@@ -14,7 +14,7 @@ var DartsUi = function (element) {
     20, 1, 18, 4, 13, 6, 10, 15, 2, 17, 3, 19, 7, 16, 8, 11, 14, 9, 12, 5
   ];
 
-  this.focusClass = 'focus';
+  this.focusClass = 'darts-focus';
 
   this.cells = {};
   this.draw();
@@ -23,17 +23,17 @@ var DartsUi = function (element) {
 };
 
 DartsUi.prototype.draw = function() {
-  var base         = this.drawCircle('base',               this.radius);
+  var base         = this.drawCircle('darts-base',               this.radius);
 
-  var doubleRings  = this.drawRings('double high-ring',    '2',   this.radius * 0.75, this.radius * 0.04);
-  var singleRingsO = this.drawRings('single single-outer', '1-o', this.radius * 0.60, this.radius * 0.25);
-  var tripleRings  = this.drawRings('triple high-ring',    '3',   this.radius * 0.45, this.radius * 0.04);
-  var singleRingsI = this.drawRings('single single-inner', '1-i', this.radius * 0.25, this.radius * 0.35);
+  var doubleRings  = this.drawRings('darts-double darts-high-ring',    '2',   this.radius * 0.75, this.radius * 0.04);
+  var singleRingsO = this.drawRings('darts-single darts-single-outer', '1-o', this.radius * 0.60, this.radius * 0.25);
+  var tripleRings  = this.drawRings('darts-triple darts-high-ring',    '3',   this.radius * 0.45, this.radius * 0.04);
+  var singleRingsI = this.drawRings('darts-single darts-single-inner', '1-i', this.radius * 0.25, this.radius * 0.35);
 
-  var OuterBull    = this.drawCircle('bull bull-outer',    this.radius * 0.1);
-  var BullsEye     = this.drawCircle('bull bull-inner',    this.radius * 0.05);
+  var OuterBull    = this.drawCircle('darts-bull darts-bull-outer',    this.radius * 0.1);
+  var BullsEye     = this.drawCircle('darts-bull darts-bull-inner',    this.radius * 0.05);
 
-  var points = this.drawPoints('point', this.radius * 0.9, this.radius * 0.1, '#fff');
+  var points = this.drawPoints('darts-point', this.radius * 0.9, this.radius * 0.1, '#fff');
 };
 
 DartsUi.prototype.drawRings = function(className, key, radius, strokeWidth) {
@@ -48,7 +48,7 @@ DartsUi.prototype.drawRings = function(className, key, radius, strokeWidth) {
     var y1 = this.centerY - radius * Math.cos(angle1);
     var ring = this.s.path('M' + x0 + ' ' + y0 + ' A' + radius + ' ' + radius + ' 0 0 1 ' + x1 + ' ' + y1);
     ring.attr({
-      class: 'cell ring',
+      class: 'darts-cell darts-ring',
       strokeWidth: strokeWidth
     });
 
@@ -63,7 +63,7 @@ DartsUi.prototype.drawRings = function(className, key, radius, strokeWidth) {
 DartsUi.prototype.drawCircle = function(className, radius) {
   var bull = this.s.circle(this.centerX, this.centerY, radius);
   bull.attr({
-    class: className + ' cell'
+    class: className + ' darts-cell'
   });
 
   return bull;
